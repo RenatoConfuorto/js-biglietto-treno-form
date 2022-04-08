568f136873cbf3747fb13c77dee71fcd4992b5ae
RenatoConfuorto <99622143+RenatoConfuorto@users.noreply.github.com>
2022-04-08 21:29:29 +0200

    show ticket completed

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const distanceInput = document.getElementById ('distance');
 const userAgeInput = document.getElementById ('user-age');
 const generateBtn = document.getElementById ('generate-btn');
 const clearBtn = document.getElementById ('clear-btn');
+const ticket = document.querySelector('.wrapper');
 
 // console.log(userName, distance, userAge, generateBtn, clearBtn);
 
@@ -25,16 +26,40 @@ generateBtn.addEventListener('click', function(){
   //calcolare il prezzo
   let price = distance * 0.21;
   let discount = 0;
+  let discountType = 'Biglietto Standard';
   console.log(price);
 
   // Calcolare lo sconto
 
   if (userAge === 'under-age') {
       discount = price * 0.2
+      discountType = 'Ridotto Minorenni';
   } else if (userAge === 'over') {
       discount = price * 0.4
+      discountType = 'Ridotto Over 65';
   }
 
   price = price - discount;
   console.log(price);
+
+  //inserire le informazione nel biglietto
+  const userNameDisplay = document.getElementById('user-name-display');
+  const discountDisplay = document.getElementById('discount');
+  const vanDisplay = document.getElementById('van');
+  const codeDisplay = document.getElementById('code');
+  const priceDisplay = document.getElementById('price');
+
+  userNameDisplay.innerHTML = userName;
+  discountDisplay.innerHTML = discountType;
+  priceDisplay.innerHTML = '€' + price.toFixed(2);
+
+  //generare numero carrozza e codice cp
+  const vanNumber = Math.floor(Math.random() * 12 + 1);
+  vanDisplay.innerHTML = vanNumber;
+
+  const codeCp = Math.floor(Math.random() * (99999 - 90000) + 90000);
+  codeDisplay.innerHTML = codeCp;
+
+  //mostrare il biglietto
+  ticket.classList.remove('d-none');
 });
